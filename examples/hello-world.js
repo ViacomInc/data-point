031bc01550196fc2fa997e3b80bae14a2dfb5d76
Acatl Pacheco <acatl.pacheco@viacom.com>
2017-10-22 13:54:26 -0400

    refactor(FunctionReducer): moves callback style up, making default sync/promise based reducer
    #10

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -2,13 +2,15 @@ const DataPoint = require('../')
 
 const dataPoint = DataPoint.create()
 
-// Reducer function that appends 'World' to the
-// value of the accumulator
+// Reducer function that appends
+// 'World' to the value of the
+// accumulator
 const reducer = acc => {
   return acc.value + ' World'
 }
 
 // applies reducer to input
 dataPoint.transform(reducer, 'Hello').then(acc => {
-  console.log(acc.value) // 'Hello World'
+  // 'Hello World'
+  console.log(acc.value)
 })
