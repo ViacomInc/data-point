5c3fd4ef020efda0da22f7d3ac59960723b48905
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-12-19 10:56:34 -0500

    refactor: Remove example.js file that was committed by accident

--- example.js
+++
@@ -1,9 +0,0 @@
-const dataPoint = require('data-point').create()
-
-dataPoint.addEntities({
-  'transform:one': acc => 'ok'
-})
-
-dataPoint.transform('transform:one', null).then(acc => console.log(acc.value))
-
-// dataPoint.transform('$a.b', null).then(acc => console.log(acc.value))
