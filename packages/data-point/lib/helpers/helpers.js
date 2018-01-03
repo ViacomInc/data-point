1bb1822273c133a1349d9cb32578395f5d1a7731
Matthew Armstrong <matthewr.armstrong@viacom.com>
2018-01-03 12:40:55 -0500

    refactor(helpers): Remove unused helper functions
    reducify, reducifyAll, mockReducer functions were not being used

--- packages/data-point/lib/helpers/helpers.js
+++ packages/data-point/lib/helpers/helpers.js
@@ -1,42 +1,8 @@
 'use strict'
 
 const _ = require('lodash')
-const Promise = require('bluebird')
 const resolveTransform = require('../transform-expression').resolve
 const AccumulatorFactory = require('../accumulator/factory')
-
-function reducify (method) {
-  return function () {
-    const partialArguments = Array.prototype.slice.call(arguments)
-    return function (acc, done) {
-      const methodArguments = [acc.value].concat(partialArguments)
-      const result = method.apply(null, methodArguments)
-      if (_.isError(result)) {
-        return done(result)
-      }
-      done(null, result)
-    }
-  }
-}
-
-module.exports.reducify = reducify
-
-function reducifyAll (source, methodList) {
-  let target = source
-  if (!_.isEmpty(methodList)) {
-    target = _.pick(source, methodList)
-  }
-  return _.mapValues(target, reducify)
-}
-
-module.exports.reducifyAll = reducifyAll
-
-function mockReducer (reducer, acc) {
-  const pcb = Promise.promisify(reducer)
-  return pcb(acc).then(val => ({ value: val }))
-}
-
-module.exports.mockReducer = mockReducer
 
 const createTransform = require('../transform-expression').create
 
