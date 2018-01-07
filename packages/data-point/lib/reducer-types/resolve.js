d719c9f61089dcd99c918662e173093ba193c7c0
Matthew Armstrong <raingerber2@gmail.com>
2018-01-07 14:49:00 -0500

    Normalize reducer resolve function signatures (#124)
    * refactor(reducer-types): Normalize the reducer resolve function signatures
    
    they now have the common signature of (manager, resolveReducer, accumulator, reducer)

--- packages/data-point/lib/reducer-types/resolve.js
+++ packages/data-point/lib/reducer-types/resolve.js
@@ -1,48 +1,16 @@
-const partial = require('lodash/partial')
+const ReducerEntity = require('./reducer-entity')
+const ReducerFunction = require('./reducer-function')
+const ReducerList = require('./reducer-list')
+const ReducerObject = require('./reducer-object')
+const ReducerPath = require('./reducer-path')
 
-const resolveReducerEntity = require('./reducer-entity').resolve
-const resolveReducerFunction = require('./reducer-function').resolve
-const resolveReducerList = require('./reducer-list').resolve
-const resolveReducerObject = require('./reducer-object').resolve
-const resolveReducerPath = require('./reducer-path').resolve
-
-/**
- * @param {Object} manager
- * @param {Function} resolveReducer
- * @param {string} reducerType
- * @returns
- */
-function getReducerFunction (manager, resolveReducer, reducerType) {
-  let resolver
-
-  /* eslint indent: ["error", 2, { "SwitchCase": 1 }] */
-  switch (reducerType) {
-    case 'ReducerPath':
-      resolver = resolveReducerPath
-      break
-    case 'ReducerFunction':
-      resolver = resolveReducerFunction
-      break
-    case 'ReducerObject':
-      // NOTE: recursive call
-      resolver = partial(resolveReducerObject, manager, resolveReducer)
-      break
-    case 'ReducerEntity':
-      // NOTE: recursive call
-      resolver = partial(resolveReducerEntity, manager, resolveReducer)
-      break
-    case 'ReducerList':
-      // NOTE: recursive call
-      resolver = partial(resolveReducerList, manager, resolveReducer)
-      break
-    default:
-      throw new Error(`Reducer type '${reducerType}' was not recognized`)
-  }
-
-  return resolver
+const resolveFunctions = {
+  [ReducerEntity.type]: ReducerEntity.resolve,
+  [ReducerFunction.type]: ReducerFunction.resolve,
+  [ReducerList.type]: ReducerList.resolve,
+  [ReducerObject.type]: ReducerObject.resolve,
+  [ReducerPath.type]: ReducerPath.resolve
 }
-
-module.exports.getReducerFunction = getReducerFunction
 
 /**
  * apply a Reducer to an accumulator
@@ -52,13 +20,20 @@ module.exports.getReducerFunction = getReducerFunction
  * @returns {Promise<Accumulator>}
  */
 function resolve (manager, accumulator, reducer) {
+  // this conditional is here because BaseEntity#resolve
+  // does not check that lifecycle methods are defined
+  // before trying to resolve them
   if (!reducer) {
     return Promise.resolve(accumulator)
   }
 
-  const reducerFunction = getReducerFunction(manager, resolve, reducer.type)
-  const result = reducerFunction(accumulator, reducer)
-  return result
+  const resolveReducer = resolveFunctions[reducer.type]
+  if (!resolveReducer) {
+    throw new Error(`Reducer type '${reducer.type}' was not recognized`)
+  }
+
+  // NOTE: recursive call
+  return resolveReducer(manager, resolve, accumulator, reducer)
 }
 
 module.exports.resolve = resolve
