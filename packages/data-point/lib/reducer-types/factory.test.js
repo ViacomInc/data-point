4ad6b275763714430a5f7b5d85d584e2929669a2
Armstrong <matthewr.armstrong@viacom.com>
2018-01-17 22:35:33 -0500

    refactor(data-point/reducer-types): Use Object.freeze in reducer-types/factory#resolve
    this removes Object.freeze from individual reducer factories
    
    #159

--- packages/data-point/lib/reducer-types/factory.test.js
+++ packages/data-point/lib/reducer-types/factory.test.js
@@ -6,6 +6,7 @@ const ReducerPath = require('./reducer-path')
 const ReducerFunction = require('./reducer-function')
 const ReducerObject = require('./reducer-object')
 const ReducerEntity = require('./reducer-entity')
+const reducerStubs = require('./reducer-helpers').stubFactories
 
 describe('reducer#create', () => {
   test('create path', () => {
@@ -45,5 +46,28 @@ describe('reducer#create', () => {
     expect(() => {
       Factory.create(1)
     }).toThrowErrorMatchingSnapshot()
+  })
+})
+
+describe('reducer#isReducer', () => {
+  function testIsReducer (source) {
+    return Factory.isReducer(Factory.create(source))
+  }
+
+  test('It should return true for reducers', () => {
+    expect(Factory.isReducer()).toBe(false)
+    expect(Factory.isReducer({})).toBe(false)
+    expect(Factory.isReducer([])).toBe(false)
+    expect(testIsReducer('entry:test')).toBe(true)
+    expect(testIsReducer(() => true)).toBe(true)
+    expect(testIsReducer(['$a', '$b'])).toBe(true)
+    expect(testIsReducer({ a: '$a' })).toBe(true)
+    expect(testIsReducer('$a.b')).toBe(true)
+    expect(testIsReducer(reducerStubs.assign('$a'))).toBe(true)
+    expect(testIsReducer(reducerStubs.filter('$a'))).toBe(true)
+    expect(testIsReducer(reducerStubs.find('$a'))).toBe(true)
+    expect(testIsReducer(reducerStubs.map('$a'))).toBe(true)
+    expect(testIsReducer(reducerStubs.omit('$a'))).toBe(true)
+    expect(testIsReducer(reducerStubs.pick('$a'))).toBe(true)
   })
 })
