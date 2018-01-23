02efbf9b7f72ac5c64482f72feb93465018a2722
Matthew Armstrong <raingerber2@gmail.com>
2018-01-23 17:05:11 -0500

    feat(data-point/entity-request): Resolve EntityRequest#options with a reducer (#163)
    * feat(data-point/entity-request): Resolve the EntityRequest#options property with a reducer
    
    this removes the TransformObject structure from existence
    
    BREAKING CHANGE: EntityRequest#options must be defined as a reducer instead of a TransformObject
    
    * feat(data-point/entity-request): Remove support for EntityRequest#beforeRequest
    
    this is no longer necessary because options is now a reducer
    
    BREAKING CHANGE: removes EntityRequest#beforeRequest from the API

--- packages/data-point/lib/entity-types/entity-request/resolve.js
+++ packages/data-point/lib/entity-types/entity-request/resolve.js
@@ -1,35 +1,8 @@
 const _ = require('lodash')
-const fp = require('lodash/fp')
 const Promise = require('bluebird')
 const rp = require('request-promise')
 
 const utils = require('../../utils')
-
-function resolveUrlInjections (url, acc) {
-  const matches = url.match(/\{(.*?)\}/g) || []
-
-  const injectedUrl = _.reduce(
-    matches,
-    (replacementTarget, match) => {
-      const objPath = match.slice(1, -1)
-      const value = _.get(acc, objPath, '')
-      return replacementTarget.replace(match, value)
-    },
-    url
-  )
-
-  return injectedUrl
-}
-
-module.exports.resolveUrlInjections = resolveUrlInjections
-
-function resolveUrl (acc) {
-  const entity = acc.reducer.spec
-  const url = resolveUrlInjections(entity.url, acc)
-  return utils.set(acc, 'url', url)
-}
-
-module.exports.resolveUrl = resolveUrl
 
 /**
  * request's default options
@@ -40,36 +13,76 @@ const REQUEST_DEFAULT_OPTIONS = {
   json: true
 }
 
-function getRequestOptions (specOptions) {
-  return _.defaults({}, specOptions, REQUEST_DEFAULT_OPTIONS)
+/**
+ * @param {string} url
+ * @param {Object} specOptions
+ * @return {Object}
+ */
+function getRequestOptions (url, specOptions) {
+  const options = _.defaults({}, specOptions, REQUEST_DEFAULT_OPTIONS)
+  // this makes it possible to modify
+  // the url in the options reducer
+  options.url = options.url || url
+  if (options.baseUrl) {
+    options.uri = options.uri || options.url
+    options.url = ''
+  }
+
+  return options
 }
 
 module.exports.getRequestOptions = getRequestOptions
 
 /**
  * Resolve options object
- * @param {Accumulator} acc
- * @param {function} resolveReducer
+ * @param {Accumulator} accumulator
+ * @param {Function} resolveReducer
+ * @return {Promise<Accumulator>}
  */
-function resolveOptions (acc, resolveReducer) {
-  const options = acc.reducer.spec.options
-  const transformOptionKeys = acc.reducer.spec.transformOptionKeys
-  // iterate over each option transform key
-  return Promise.reduce(
-    transformOptionKeys,
-    (newOptions, key) => {
-      return resolveReducer(acc, key.transform).then(res => {
-        return fp.set(key.path, res.value, newOptions)
-      })
-    },
-    options
-  ).then(resolvedOptions => {
-    return utils.set(acc, 'options', resolvedOptions)
+function resolveOptions (accumulator, resolveReducer) {
+  accumulator = resolveUrl(accumulator)
+  const specOptions = accumulator.reducer.spec.options
+  return resolveReducer(accumulator, specOptions).then(acc => {
+    const options = getRequestOptions(acc.url, acc.value)
+    return utils.assign(accumulator, { options })
   })
 }
 
 module.exports.resolveOptions = resolveOptions
 
+/**
+ * @param {string} url
+ * @param {Accumulator} acc
+ * @return {string}
+ */
+function resolveUrlInjections (url, acc) {
+  const matches = url.match(/\{(.*?)\}/g) || []
+  const injectedUrl = matches.reduce((replacementTarget, match) => {
+    const objPath = match.slice(1, -1)
+    const value = _.get(acc, objPath, '')
+    return replacementTarget.replace(match, value)
+  }, url)
+
+  return injectedUrl
+}
+
+module.exports.resolveUrlInjections = resolveUrlInjections
+
+/**
+ * @param {Accumulator} acc
+ * @return {Accumulator}
+ */
+function resolveUrl (acc) {
+  const entity = acc.reducer.spec
+  const url = resolveUrlInjections(entity.url, acc)
+  return utils.assign(acc, { url })
+}
+
+module.exports.resolveUrl = resolveUrl
+
+/**
+ * @param {Accumulator} acc
+ */
 function inspect (acc) {
   if (acc.params && acc.params.inspect) {
     utils.inspect(acc, {
@@ -78,30 +91,14 @@ function inspect (acc) {
     })
   }
 }
+
 module.exports.inspect = inspect
 
-function resolveBeforeRequest (acc, resolveReducer) {
-  const entity = acc.reducer.spec
-
-  let options = getRequestOptions(acc.options)
-
-  options = utils.set(options, 'url', acc.url)
-
-  if (options.baseUrl && !options.uri) {
-    options = Object.assign({}, options, {
-      uri: acc.url,
-      url: ''
-    })
-  }
-
-  const beforeRequestAcc = utils.set(acc, 'value', options)
-  return resolveReducer(beforeRequestAcc, entity.beforeRequest).then(result =>
-    utils.set(acc, 'options', result.value)
-  )
-}
-
-module.exports.resolveBeforeRequest = resolveBeforeRequest
-
+/**
+ * @param {Accumulator} acc
+ * @param {Function} resolveReducer
+ * @return {Promise<Accumulator>}
+ */
 function resolveRequest (acc, resolveReducer) {
   inspect(acc)
   return rp(acc.options)
@@ -133,13 +130,16 @@ function resolveRequest (acc, resolveReducer) {
 
 module.exports.resolveRequest = resolveRequest
 
+/**
+ * @param {Accumulator} acc
+ * @param {Function} resolveReducer
+ * @return {Promise<Accumulator>}
+ */
 function resolve (acc, resolveReducer) {
   const entity = acc.reducer.spec
   return Promise.resolve(acc)
     .then(itemContext => resolveReducer(itemContext, entity.value))
     .then(itemContext => resolveOptions(itemContext, resolveReducer))
-    .then(itemContext => resolveUrl(itemContext))
-    .then(itemContext => resolveBeforeRequest(itemContext, resolveReducer))
     .then(itemContext => resolveRequest(itemContext, resolveReducer))
 }
 
