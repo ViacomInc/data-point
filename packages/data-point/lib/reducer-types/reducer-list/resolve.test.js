c123a11eac0830aafe3fa738b7a5172107bce221
Victor Singh <victor.singh03@qmail.cuny.edu>
2018-01-24 08:25:28 -0500

    test(data-point): remove duplicate tests (#147)
    Corrected and clarified descriptions of tests
    
    Closes #105

--- packages/data-point/lib/reducer-types/reducer-list/resolve.test.js
+++ packages/data-point/lib/reducer-types/reducer-list/resolve.test.js
@@ -80,7 +80,7 @@ describe('resolve#reducer.resolve - reducer model', () => {
     ).then(result => expect(result.value).toEqual(testData))
   })
 
-  test('multiple models', () => {
+  test('it returns original input after piping through hash:asIs', () => {
     const accumulator = AccumulatorFactory.create({
       value: testData
     })
@@ -92,7 +92,9 @@ describe('resolve#reducer.resolve - reducer model', () => {
       resolveReducer,
       accumulator,
       reducerList
-    ).then(result => expect(result.value).toEqual(testData.a.h))
+    ).then(result => {
+      expect(result.value).toEqual(testData.a.h)
+    })
   })
 })
 
@@ -122,7 +124,7 @@ describe('resolve#reducer.resolve - reducer request', () => {
     )
   })
 
-  test('multiple models', () => {
+  test('multiple models for reducer request', () => {
     nock('http://remote.test')
       .get('/source1')
       .reply(200, {
@@ -146,10 +148,10 @@ describe('resolve#reducer.resolve - reducer request', () => {
       resolveReducer,
       accumulator,
       reducer
-    ).then(result =>
+    ).then(result => {
       expect(result.value).toEqual({
         ok: true
       })
-    )
+    })
   })
 })
