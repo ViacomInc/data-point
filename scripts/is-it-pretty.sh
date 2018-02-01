a7b01473ac4162a24c98789d0a9204a9d43a3a2d
Victor <vicsingh305@gmail.com>
2018-02-01 10:41:19 -0500

    ci(scripts/is-it-pretty.sh): Added a detailed description on why travis ci fails
    #142

--- scripts/is-it-pretty.sh
+++ scripts/is-it-pretty.sh
@@ -3,10 +3,9 @@
 node_modules/.bin/prettier-standard '**/*.js'
 if [ -z "$(git status --porcelain)" ]; then
   # Working directory clean
-  echo 'OK'
   exit 0
 else
   # Uncommitted changes
-  echo 'BAD'
+  echo 'There are javascript files in this project that do not follow the prettify standard!'
   exit 1
 fi
