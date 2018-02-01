8d85276a732c357ce95bc04023bb4d505d749056
acatl pacheco <acatl.pacheco@gmail.com>
2018-02-01 15:42:14 -0500

    ci(scripts/is-it-pretty.sh) change wording

--- scripts/is-it-pretty.sh
+++ scripts/is-it-pretty.sh
@@ -6,7 +6,9 @@ if [ -z "$(git status --porcelain)" ]; then
   exit 0
 else
   # Uncommitted changes
-  echo 'There are javascript files in this project that do not follow the prettify standard!'
+  echo 'There are javascript files in this project that do not pass prettier-standard.'
+  echo 'To avoid this error make commits by using the "yarn run commit" command.'
+  echo 'For more info: https://github.com/ViacomInc/data-point/blob/master/CONTRIBUTING.md\n'
   echo "$(git diff --name-only)"
   exit 1
 fi
