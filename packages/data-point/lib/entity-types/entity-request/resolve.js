b8fe51db9c6a389053beb680de80f39ff2070f4b
Victor <vicsingh305@gmail.com>
2018-02-13 13:01:44 -0500

    feat(packages/data-point/lib/entity-type/entity-request/resolve.js): Added a parameter to resolveReq
    #195

--- packages/data-point/lib/entity-types/entity-request/resolve.js
+++ packages/data-point/lib/entity-types/entity-request/resolve.js
@@ -98,9 +98,10 @@ module.exports.inspect = inspect
 /**
  * @param {Accumulator} acc
  * @param {Function} resolveReducer
+ * @param {boolean} showErr
  * @return {Promise<Accumulator>}
  */
-function resolveRequest (acc, resolveReducer) {
+function resolveRequest (acc, resolveReducer, showErr) {
   inspect(acc)
   return rp(acc.options)
     .then(result => utils.set(acc, 'value', result))
@@ -129,7 +130,7 @@ function resolveRequest (acc, resolveReducer) {
 
       // this is useful in the case the error itself is not logged by the
       // implementation
-      console.info(redactedError.toString(), message)
+      if (showErr) console.info(redactedError.toString(), message)
 
       // attaching to error so it can be exposed by a handler outside datapoint
       error.message = `${error.message}\n\n${message}`
