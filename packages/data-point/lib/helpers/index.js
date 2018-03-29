7a6694f2fe78b8b3c8e1eccadce9823f21276b2e
Matthew Armstrong <matthewr.armstrong@viacom.com>
2018-03-29 15:55:57 -0400

    fix(data-point): Add back missing entityFactories object

--- packages/data-point/lib/helpers/index.js
+++ packages/data-point/lib/helpers/index.js
@@ -1,26 +1,3 @@
 /* eslint global-require: 0 */
 
-const helpers = require('./helpers')
-
-const utils = require('../utils')
-
-module.exports = {
-  helpers: helpers.helpers,
-  isReducer: helpers.isReducer,
-  createReducer: helpers.createReducer,
-  createEntity: helpers.createEntity,
-  resolveEntity: helpers.resolveEntity,
-  validateEntityModifiers: helpers.validateEntityModifiers,
-  reducify: helpers.reducify,
-  reducifyAll: helpers.reducifyAll,
-  mockReducer: helpers.mockReducer,
-  createAccumulator: helpers.createAccumulator,
-  createReducerResolver: helpers.createReducerResolver,
-  set: utils.set,
-  assign: utils.assign,
-  getUID: utils.getUID,
-  typeOf: utils.typeOf,
-  inspect: utils.inspect,
-  inspectProperties: utils.inspectProperties,
-  isFalsy: utils.isFalsy
-}
+module.exports = Object.assign({}, require('./helpers'), require('../utils'))
