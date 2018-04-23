541983dd35c796cdaf9984776ca7bae93d960bff
Matthew Armstrong <raingerber2@gmail.com>
2018-04-24 02:03:52 +0800

    refactor(middleware): Store middleware in a map (#232)
    * refactor(middleware): Store middleware in a map instead of an array
    * test(setup-middleware.test.js): Add more expects to middleware test
    
    closes #231

--- packages/data-point/lib/middleware-context/factory.js
+++ packages/data-point/lib/middleware-context/factory.js
@@ -1,18 +1,25 @@
-/* eslint no-underscore-dangle: ["error", { "allow": ["___done", "___resolve"] }] */
-
 const _ = require('lodash')
 
+/**
+ * @class
+ * @property {Boolean} ___done
+ * @property {Boolean} ___resolve
+ */
 function MiddlewareContext () {
   this.___done = false
   this.___resolve = false
 }
 
+/**
+ * @param {*} value
+ * @throws if it's been called more than once
+ */
 MiddlewareContext.prototype.resolve = function resolve (value) {
   if (this.___resolve === true || this.___done === true) {
     throw new Error('can not execute resolve() more than once per stack chain.')
   }
 
-  // only assign if argument passed regardless of the value
+  // only assign if an argument is passed, regardless of the value
   if (arguments.length) {
     this.value = value
     this.___resolve = true
@@ -23,6 +30,10 @@ MiddlewareContext.prototype.resolve = function resolve (value) {
 
 module.exports.MiddlewareContext = MiddlewareContext
 
+/**
+ * @param {Object} spec
+ * @return {Object}
+ */
 function create (spec) {
   const middlewareContext = new MiddlewareContext()
   const middlewareContextExtended = _.assignIn(middlewareContext, spec)
