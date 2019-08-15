8f07f00aca5a70e5ff73904a412fef462f506150
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-15 15:14:42 -0400

    fix: pass linters and tests for initial state
    removes support for 8
    
    closes #378

--- scripts/preinstall-script.js
+++ scripts/preinstall-script.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-const setup =
-  '\nhttps://github.com/ViacomInc/data-point/blob/master/CONTRIBUTING.md#setup\n'
+/* eslint-disable no-console */
 
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
+const setup =
+  "\nhttps://github.com/ViacomInc/data-point/blob/master/CONTRIBUTING.md#setup\n";
+
+if (process.env.npm_execpath.indexOf("yarn") === -1) {
   console.error(
-    '\x1b[41m',
-    'Error: You must use yarn to install dependencies.\n\n',
-    'For help setting up this project please vist:',
-    '\x1b[0m',
-    '\x1b[36m',
-    '\n',
+    "\x1b[41m",
+    "Error: You must use yarn to install dependencies.\n\n",
+    "For help setting up this project please vist:",
+    "\x1b[0m",
+    "\x1b[36m",
+    "\n",
     setup
-  )
-  process.exit(1)
+  );
+  process.exit(1);
 }
