c47f710d4fbfacc8c7b15ab08fcd02a17fe58c8d
Acatl <acatl.pacheco@gmail.com>
2019-08-15 14:58:55 -0400

    style: pass linters and tests for initial state

--- scripts/preinstall-script.js
+++ scripts/preinstall-script.js
@@ -1,17 +1,19 @@
-#!/usr/bin/env node
+#!/usr/bin/env node;
+
+/* eslint-disable no-console */
 
 const setup =
-  '\nhttps://github.com/ViacomInc/data-point/blob/master/CONTRIBUTING.md#setup\n'
+  "\nhttps://github.com/ViacomInc/data-point/blob/master/CONTRIBUTING.md#setup\n";
 
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
+if (process.env.npm_execpath.indexOf("yarn") === -1) {
   console.error(
-    '\x1b[41m',
-    'Error: You must use yarn to install dependencies.\n\n',
-    'For help setting up this project please vist:',
-    '\x1b[0m',
-    '\x1b[36m',
-    '\n',
+    "\x1b[41m",
+    "Error: You must use yarn to install dependencies.\n\n",
+    "For help setting up this project please vist:",
+    "\x1b[0m",
+    "\x1b[36m",
+    "\n",
     setup
-  )
-  process.exit(1)
+  );
+  process.exit(1);
 }
