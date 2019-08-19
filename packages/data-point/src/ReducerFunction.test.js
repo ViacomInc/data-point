e14354c43b58e966db1edc997f5f71fd52d09650
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-19 16:27:47 -0400

    test(data-point): add tests

---
+++ packages/data-point/src/ReducerFunction.test.js
@@ -0,0 +1,59 @@
+const { ReducerFunction } = require("./ReducerFunction");
+const { Accumulator } = require("./Accumulator");
+
+describe("ReducerFunction", () => {
+  describe("constructor", () => {
+    const fn = () => true;
+
+    it("should set functionBody property", () => {
+      const result = new ReducerFunction(fn);
+      expect(result.functionBody).toEqual(fn);
+    });
+
+    it("should use function's name as the reducer's name", () => {
+      const result = new ReducerFunction(fn);
+      expect(result.name).toEqual("fn");
+    });
+
+    it("should use anonymous as the reducer's name if function does not have a name", () => {
+      const result = new ReducerFunction(() => true);
+      expect(result.name).toEqual("anonymous");
+    });
+
+    it("should set reducer's type to 'function'", () => {
+      const result = new ReducerFunction(fn);
+      expect(result.type).toEqual("function");
+    });
+  });
+
+  describe("isType", () => {
+    it("should be accessible as static method", () => {
+      expect(ReducerFunction.isType).toBeInstanceOf(Function);
+    });
+
+    it("should return true for function reference", () => {
+      expect(ReducerFunction.isType(() => true)).toEqual(true);
+      expect(ReducerFunction.isType({})).toEqual(false);
+      expect(ReducerFunction.isType("string")).toEqual(false);
+      expect(ReducerFunction.isType([])).toEqual(false);
+    });
+  });
+
+  describe("resolve", () => {
+    it("should execute function", async () => {
+      const fn = jest.fn(() => "result");
+      const reducer = new ReducerFunction(fn);
+      const acc = new Accumulator({
+        value: "val"
+      });
+      const result = await reducer.resolve(acc);
+
+      // return function's result
+      expect(result).toEqual("result");
+      // pass value and accumulator as parameters
+      expect(fn).toBeCalledWith(acc.value, acc);
+      // to use undefined as the value of "this"
+      expect(fn.mock.instances).toEqual([undefined]);
+    });
+  });
+});
