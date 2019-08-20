3df3e062d6a4c5b0ddd745f0c9ad285f29d603eb
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-20 12:52:29 -0400

    feat(data-point): ReducerPath features and tests
    add tests and port compatibility with legacy data-point

--- packages/data-point/src/ReducerPath.js
+++ packages/data-point/src/ReducerPath.js
@@ -1,36 +1,94 @@
-const { ReducerNative } = require("./ReducerNative");
+const { Reducer } = require("./Reducer");
 
 // Credit:
+// path parsing based off of the work on:
 // https://gomakethings.com/how-to-get-the-value-of-an-object-from-a-specific-path-with-vanilla-js/
 // chris@gomakethings.com
 
-const splitToArrayRegEx = /\[([^}]+)\]/g;
+/**
+ * @type {RegExp} matches simple accessor separators `[`, `]` and `.`
+ */
+const splitToArrayRegEx = /[[,.,\]]/g;
 
-function stringToPath(path) {
+/**
+ * @param {string} pathProperty
+ * @returns {number|string} parsed value
+ */
+function parsePathProperty(pathProperty) {
+  const numberValue = Number(pathProperty);
+  return Number.isNaN(numberValue) ? pathProperty : numberValue;
+}
+
+/**
+ * @param {string} pathSource
+ * @returns {Array<string|number>}
+ */
+function stringToPath(pathSource) {
   const output = [];
 
-  // Split to an array with dot notation
-  path.split(".").forEach(item => {
-    // Split to an array with bracket notation
-    item.split(splitToArrayRegEx).forEach(key => {
-      // Push to the new array
-      if (key.length > 0) {
-        output.push(key);
-      }
-    });
-  });
+  const accessors = pathSource.split(splitToArrayRegEx);
+
+  const accessorsLength = accessors.length;
+
+  // using for loop for faster iteration
+  for (let index = 0; index < accessorsLength; index += 1) {
+    const accessor = accessors[index];
+    if (accessor !== "") {
+      output.push(parsePathProperty(accessor));
+    }
+  }
 
   return output;
 }
 
-function getPath(value, path) {
+/**
+ * @param {string} pathSource
+ * @returns {Object} compiled source
+ */
+function parsePath(pathSource) {
+  const compliedSource = {
+    /**
+     * @type {Array<string|number>} parsed object path
+     */
+    path: [],
+    /**
+     * @type {Boolean} true if crawling should look one level up
+     */
+    moveUp: false
+  };
+
+  let sourceToCompile = pathSource.substr(1);
+
+  if (pathSource.startsWith("$..")) {
+    compliedSource.moveUp = true;
+    sourceToCompile = sourceToCompile.substr(2);
+  }
+
+  if (pathSource === "$.") {
+    compliedSource.path = [];
+  } else {
+    compliedSource.path = stringToPath(sourceToCompile);
+  }
+
+  return compliedSource;
+  // return pathSource === "$." ? [] : stringToPath(pathSource.substr(1));
+}
+
+/**
+ * @param {any} value value to crawl
+ * @param {Array<string|number>} path object path to crawl
+ * @returns {any} resolved path
+ */
+function crawlPath(value, path) {
   let current = value;
 
   // For each item in the path, dig into the object
   const length = path.length;
+
+  // using for loop for faster iteration
   for (let i = 0; i < length; i += 1) {
     // If the item isn't found, return undefined
-    if (!current[path[i]]) return undefined;
+    if (current[path[i]] === undefined) return undefined;
 
     // Otherwise, update the current  value
     current = current[path[i]];
@@ -39,21 +97,54 @@ function getPath(value, path) {
   return current;
 }
 
-class ReducerPath extends ReducerNative {
+/**
+ * @param {Accumulator} accumulator
+ * @param {Object} compiledPath
+ * @returns {any} resolved value
+ */
+function resolvePath(accumulator, compiledPath) {
+  const valueToCrawl =
+    compiledPath.moveUp === true ? accumulator : accumulator.value;
+
+  return crawlPath(valueToCrawl, compiledPath.path);
+}
+
+/**
+ * Reducer that crawls a data path (using dot notation) from the accumulator
+ */
+class ReducerPath extends Reducer {
+  /**
+   * @param {string} spec dot notation object path, expects path to start with $
+   */
   constructor(spec) {
     super("path", spec, spec);
-    this.path = stringToPath(spec.substr(1));
+    this.compiledPath = parsePath(spec);
   }
 
+  /**
+   * @param {string} spec
+   */
   static isType(spec) {
-    return typeof spec === "string" && spec.charAt(0) === "$";
+    return (
+      typeof spec === "string" && spec.charAt(0) === "$" && spec.length > 1
+    );
   }
 
+  /**
+   *
+   * @param {Accumulator} accumulator
+   */
   resolve(accumulator) {
-    return getPath(accumulator.value, this.path);
+    return resolvePath(accumulator, this.compiledPath);
   }
 }
 
 module.exports = {
+  splitToArrayRegEx,
+  parsePathProperty,
+  stringToPath,
+  parsePath,
+  crawlPath,
+  resolvePath,
   ReducerPath
 };
---
+++ packages/data-point/src/ReducerPath.test.js
@@ -0,0 +1,209 @@
+const reducerPath = require("./ReducerPath");
+const { Accumulator } = require("./Accumulator");
+
+const value = {
+  a: {
+    b: [
+      [
+        {
+          c: "c"
+        }
+      ]
+    ]
+  },
+  e: ["", false, null, 0]
+};
+
+describe("splitToArrayRegEx", () => {
+  it("should split dots and bracket notation", () => {
+    const testString = "aaa.11.rt.43.[1].a.[8][abc][9]";
+    const result = testString.split(reducerPath.splitToArrayRegEx);
+    expect(result).toEqual([
+      "aaa",
+      "11",
+      "rt",
+      "43",
+      "",
+      "1",
+      "",
+      "a",
+      "",
+      "8",
+      "",
+      "abc",
+      "",
+      "9",
+      ""
+    ]);
+  });
+});
+
+describe("parsePathProperty", () => {
+  it("should leave as string if value cant be converted to number", () => {
+    expect(reducerPath.parsePathProperty("ab2")).toEqual("ab2");
+  });
+
+  it("should parse to number if string can be parsed to number", () => {
+    expect(reducerPath.parsePathProperty("2")).toEqual(2);
+  });
+});
+
+describe("stringToPath", () => {
+  it("should parse dot notation into array of strings and numbers", () => {
+    expect(reducerPath.stringToPath("a.b.c[9][7][bb].r[o]")).toEqual([
+      "a",
+      "b",
+      "c",
+      9,
+      7,
+      "bb",
+      "r",
+      "o"
+    ]);
+  });
+});
+
+describe("parsePath", () => {
+  it("should parse $.", () => {
+    expect(reducerPath.parsePath("$.")).toMatchObject({
+      moveUp: false,
+      path: []
+    });
+  });
+
+  it("should parse $..", () => {
+    expect(reducerPath.parsePath("$..")).toMatchObject({
+      moveUp: true,
+      path: []
+    });
+
+    expect(reducerPath.parsePath("$..locals.a")).toMatchObject({
+      moveUp: true,
+      path: ["locals", "a"]
+    });
+  });
+
+  it("should parse $a.b", () => {
+    expect(reducerPath.parsePath("$a.b")).toMatchObject({
+      moveUp: false,
+      path: ["a", "b"]
+    });
+  });
+});
+
+describe("crawlPath", () => {
+  it("should get current value if array is empty", () => {
+    expect(reducerPath.crawlPath(value, [])).toEqual(value);
+  });
+
+  it("should get value inside path", () => {
+    expect(reducerPath.crawlPath(value, ["a"])).toEqual(value.a);
+    expect(reducerPath.crawlPath(value, ["a", "b"])).toEqual(value.a.b);
+    expect(reducerPath.crawlPath(value, ["a", "b", 0, 0])).toEqual(
+      value.a.b[0][0]
+    );
+    expect(reducerPath.crawlPath(value, ["a", "b", 0, 0, "c"])).toEqual(
+      value.a.b[0][0].c
+    );
+  });
+
+  it("should resolve properties of values", () => {
+    expect(reducerPath.crawlPath(value, ["e", "length"])).toEqual(
+      value.e.length
+    );
+    expect(reducerPath.crawlPath(value, ["e", 0, "length"])).toEqual(
+      value.e[0].length
+    );
+  });
+
+  it("should resolve falsy values", () => {
+    // empty string
+    expect(reducerPath.crawlPath(value, ["e", 0])).toEqual(value.e[0]);
+    // false
+    expect(reducerPath.crawlPath(value, ["e", 1])).toEqual(value.e[1]);
+    // null
+    expect(reducerPath.crawlPath(value, ["e", 2])).toEqual(value.e[2]);
+    // 0
+    expect(reducerPath.crawlPath(value, ["e", 3])).toEqual(value.e[3]);
+  });
+
+  it("should return undefined for invalid values", () => {
+    expect(reducerPath.crawlPath(value, ["a", "invalid"])).toEqual(undefined);
+    // out of range index
+    expect(reducerPath.crawlPath(value, ["e", 5])).toEqual(undefined);
+  });
+});
+
+describe("resolvePath", () => {
+  it("should use accumulator if moveUp flag is true", () => {
+    const acc = new Accumulator({
+      value,
+      locals: {
+        myLocal: "myLocal"
+      }
+    });
+
+    const compiledPath = {
+      moveUp: true,
+      path: ["locals", "myLocal"]
+    };
+    expect(reducerPath.resolvePath(acc, compiledPath)).toEqual("myLocal");
+  });
+
+  it("should use accumulator.value if moveUp flag is false", () => {
+    const acc = new Accumulator({
+      value
+    });
+
+    const compiledPath = {
+      moveUp: false,
+      path: ["a", "b"]
+    };
+    expect(reducerPath.resolvePath(acc, compiledPath)).toEqual(value.a.b);
+  });
+});
+
+describe("ReducerPath", () => {
+  const ReducerPath = reducerPath.ReducerPath;
+  const pathSrc = "$a.b[1][5].c";
+  describe("constructor", () => {
+    it("should set compiledPath property", () => {
+      const result = new ReducerPath(pathSrc);
+      expect(result.compiledPath).toMatchObject({
+        moveUp: false,
+        path: ["a", "b", 1, 5, "c"]
+      });
+    });
+
+    it("should use function's name as the reducer's name", () => {
+      const result = new ReducerPath(pathSrc);
+      expect(result.name).toEqual(pathSrc);
+    });
+
+    it("should set reducer's type to 'function'", () => {
+      const result = new ReducerPath(pathSrc);
+      expect(result.type).toEqual("path");
+    });
+  });
+
+  describe("isType", () => {
+    it("should check is path", () => {
+      expect(ReducerPath.isType(0)).toEqual(false);
+      expect(ReducerPath.isType("")).toEqual(false);
+      expect(ReducerPath.isType("$")).toEqual(false);
+      expect(ReducerPath.isType("$.")).toEqual(true);
+      expect(ReducerPath.isType("$1")).toEqual(true);
+      expect(ReducerPath.isType("$a")).toEqual(true);
+    });
+  });
+
+  describe("resolve", () => {
+    it("should resolve compiled string from accumulator's value", () => {
+      const result = new ReducerPath("$a.b");
+      const acc = new Accumulator({
+        value
+      });
+      expect(result.resolve(acc)).toEqual(value.a.b);
+    });
+  });
+});
