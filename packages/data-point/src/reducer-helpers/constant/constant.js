539bec4138270ba0989c7aa831164b6f261c45d4
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-22 14:16:20 -0400

    * feat(data-point): only cloneDeep when necesary (#387)
    - move isPlainObject as a module
    
    closes #386

--- packages/data-point/src/reducer-helpers/constant/constant.js
+++ packages/data-point/src/reducer-helpers/constant/constant.js
@@ -1,14 +1,24 @@
 const cloneDeep = require("lodash/cloneDeep");
 
 const { Reducer } = require("../../Reducer");
+const isPlainObject = require("../../is-plain-object");
+
+/**
+ * @param {Boolean} isObject true if parameter `value` is assumed to be an object
+ * @param {any} value
+ */
+function cloneObject(isObject, value) {
+  return isObject ? cloneDeep(value) : value;
+}
 
 class ReducerConstant extends Reducer {
   constructor(spec) {
     super("constant", undefined, spec);
+    this.isPlainObject = isPlainObject(spec);
     // We clone the source to truly respect the nature of a constant, if the
     // original source is ever changed the data integrity of the constant
     // should be kept
-    this.constantValue = cloneDeep(spec);
+    this.constantValue = cloneObject(this.isPlainObject, spec);
   }
 
   static create(spec) {
@@ -21,10 +31,11 @@ class ReducerConstant extends Reducer {
   resolve() {
     // this clone happens so any future changes to the output do not
     // affect the original value of this.constantValue
-    return cloneDeep(this.constantValue);
+    return cloneObject(this.isPlainObject, this.constantValue);
   }
 }
 
 module.exports = {
+  cloneObject,
   ReducerConstant
 };
