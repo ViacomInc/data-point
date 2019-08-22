be33c4d45b024315ebf94f1cb8fcc39ff1c47c22
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-22 16:53:59 -0400

    test(data-point): add tests for reducer map
    closes #391

---
+++ packages/data-point/src/reducer-helpers/map/map.test.js
@@ -0,0 +1,38 @@
+const { ReducerMap } = require("./map");
+const { Accumulator } = require("../../Accumulator");
+const { resolve } = require("../../resolve");
+
+describe("ReducerMap", () => {
+  const spec = value => value * 2;
+
+  describe("constructor", () => {
+    it("should set type to 'constant'", () => {
+      const reducer = new ReducerMap(spec);
+      expect(reducer.type).toEqual("map");
+    });
+
+    it("should create a reducer from spec and assign to mapReducer", () => {
+      const reducer = new ReducerMap(spec);
+      expect(reducer.mapReducer).toHaveProperty("type", "function");
+    });
+  });
+
+  describe("create", () => {
+    it("should create a new instance of ReducerMap", () => {
+      expect(ReducerMap.create(spec)).toBeInstanceOf(ReducerMap);
+    });
+  });
+
+  describe("resolve", () => {
+    it("should map reducers to each element on the provided array", async () => {
+      const reducer = new ReducerMap(spec);
+
+      const acc = new Accumulator({
+        value: [1, 2, 3]
+      });
+
+      const result = await reducer.resolve(acc, resolve);
+      expect(result).toEqual([2, 4, 6]);
+    });
+  });
+});
