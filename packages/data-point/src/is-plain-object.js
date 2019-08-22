091756bcd1128baa2e6f490a0c9e7c62697d4d73
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-22 12:58:07 -0400

    feat(data-point): move isPlainObject as a module

---
+++ packages/data-point/src/is-plain-object.js
@@ -0,0 +1,6 @@
+function isPlainObject(obj) {
+  return (
+    typeof obj === "object" && Object.getPrototypeOf(obj) === Object.prototype
+  );
+}
+module.exports = isPlainObject;
---
+++ packages/data-point/src/is-plain-object.test.js
@@ -0,0 +1,14 @@
+const isPlainObject = require("./is-plain-object");
+
+describe("isPlainObject", () => {
+  it("should return true for plain objects", () => {
+    expect(isPlainObject({})).toEqual(true);
+    expect(isPlainObject("string")).toEqual(false);
+    expect(isPlainObject(123)).toEqual(false);
+    expect(isPlainObject(() => true)).toEqual(false);
+
+    expect(isPlainObject(Object.create({}))).toEqual(false);
+    class Test {}
+    expect(isPlainObject(new Test())).toEqual(false);
+  });
+});
