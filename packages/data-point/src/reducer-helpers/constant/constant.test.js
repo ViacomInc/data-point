e423f97580b3911e38685f976865561eb44e61f3
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-22 13:00:00 -0400

    feat(data-point): only cloneDeep when necesary

--- packages/data-point/src/reducer-helpers/constant/constant.test.js
+++ packages/data-point/src/reducer-helpers/constant/constant.test.js
@@ -1,4 +1,31 @@
-const { ReducerConstant } = require("./constant");
+jest.mock("lodash/cloneDeep");
+const mockCloneDeep = require("lodash/cloneDeep");
+
+mockCloneDeep.mockImplementation(value => {
+  return jest.requireActual("lodash/cloneDeep")(value);
+});
+
+const { ReducerConstant, cloneObject } = require("./constant");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("cloneObject", () => {
+  it("should not clone if parameter isObject is passed as false", () => {
+    const result = cloneObject(false, "string");
+    expect(mockCloneDeep).not.toBeCalled();
+    expect(result).toEqual("string");
+  });
+
+  it("should clone if parameter isObject is passed as true", () => {
+    const source = {};
+    const result = cloneObject(true, source);
+    expect(mockCloneDeep).toBeCalled();
+    source.mutated = true;
+    expect(result).toEqual({});
+  });
+});
 
 describe("ReducerConstant", () => {
   describe("constructor", () => {
@@ -10,6 +37,13 @@ describe("ReducerConstant", () => {
     it("should set constantValue to source received", () => {
       const reducer = new ReducerConstant("value");
       expect(reducer.constantValue).toEqual("value");
+    });
+
+    it("should not clone non-plain objects", () => {
+      const reducer = new ReducerConstant(true);
+
+      expect(reducer.constantValue).toEqual(true);
+      expect(mockCloneDeep).not.toBeCalled();
     });
 
     it("should set constantValue to be deep cloned from source received", () => {
@@ -44,7 +78,7 @@ describe("ReducerConstant", () => {
       expect(reducer.resolve()).toEqual(sourceObject);
     });
 
-    it("should return deep cloned value", () => {
+    it("should return deep cloned value for plain objects", () => {
       const sourceObject = {
         test: "test"
       };
@@ -56,6 +90,13 @@ describe("ReducerConstant", () => {
       expect(sourceObject).toEqual({
         test: "test"
       });
+    });
+
+    it("should return value for non-plain objects", () => {
+      const reducer = ReducerConstant.create("string");
+      const result = reducer.resolve();
+      expect(result).toEqual("string");
+      expect(mockCloneDeep).not.toBeCalled();
     });
   });
 });
