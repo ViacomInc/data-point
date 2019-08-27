3203655a9f827dfca6c475b0908764c83599040c
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-27 08:45:49 -0400

    test(data-point): rename variable

--- packages/data-point/src/reducer-helpers/map/map.test.js
+++ packages/data-point/src/reducer-helpers/map/map.test.js
@@ -3,29 +3,29 @@ const { Accumulator } = require("../../Accumulator");
 const { resolve } = require("../../resolve");
 
 describe("ReducerMap", () => {
-  const spec = value => value * 2;
+  const multiplyByTwo = value => value * 2;
 
   describe("constructor", () => {
     it("should set type to 'map'", () => {
-      const reducer = new ReducerMap(spec);
+      const reducer = new ReducerMap(multiplyByTwo);
       expect(reducer.type).toEqual("map");
     });
 
     it("should create a reducer from spec and assign to iterateeReducer", () => {
-      const reducer = new ReducerMap(spec);
+      const reducer = new ReducerMap(multiplyByTwo);
       expect(reducer.iterateeReducer).toHaveProperty("type", "function");
     });
   });
 
   describe("create", () => {
     it("should create a new instance of ReducerMap", () => {
-      expect(ReducerMap.create(spec)).toBeInstanceOf(ReducerMap);
+      expect(ReducerMap.create(multiplyByTwo)).toBeInstanceOf(ReducerMap);
     });
   });
 
   describe("resolve", () => {
     it("should map reducers to each element on the provided array", async () => {
-      const reducer = new ReducerMap(spec);
+      const reducer = new ReducerMap(multiplyByTwo);
 
       const acc = new Accumulator({
         value: [1, 2, 3]
