2fd412e423059b80e1560f0aa980b4ee36b52dd1
Acatl Pacheco <acatl.pacheco@gmail.com>
2019-08-29 09:34:16 -0400

    fix(data-point): fix check against null (#403)
    - fix check against null
    - adds a description to the method

--- packages/data-point/src/is-plain-object.js
+++ packages/data-point/src/is-plain-object.js
@@ -1,6 +1,19 @@
+/**
+ * This method is used to check if the argument passed is a simple object.
+ *
+ * Please take a look at the unit tests of this module to understand which
+ * values are considered an object.
+ *
+ * If there are more cases that need to be considered please create an issue.
+ *
+ * @param {any} obj
+ * @returns {Boolean} true if object created via `{}`
+ */
 function isPlainObject(obj) {
   return (
-    typeof obj === "object" && Object.getPrototypeOf(obj) === Object.prototype
+    obj !== null &&
+    typeof obj === "object" &&
+    Object.getPrototypeOf(obj) === Object.prototype
   );
 }
 module.exports = isPlainObject;
--- packages/data-point/src/is-plain-object.test.js
+++ packages/data-point/src/is-plain-object.test.js
@@ -5,6 +5,8 @@ describe("isPlainObject", () => {
     expect(isPlainObject({})).toEqual(true);
     expect(isPlainObject("string")).toEqual(false);
     expect(isPlainObject(123)).toEqual(false);
+    expect(isPlainObject(null)).toEqual(false);
+    expect(isPlainObject([])).toEqual(false);
     expect(isPlainObject(() => true)).toEqual(false);
 
     expect(isPlainObject(Object.create({}))).toEqual(false);
