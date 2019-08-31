344be8eb546e1f7dc23a673d5b5977cfc9e47d31
Christian Sarnataro <csarnataro@users.noreply.github.com>
2019-08-31 17:33:36 +0200

    docs(JSDoc): integrate JSDoc in Docusaurus (#411)
    closes #410

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -9,16 +9,6 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: "User1",
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: "/img/undraw_open_source.svg",
-    infoLink: "https://www.facebook.com",
-    pinned: true
-  }
-];
 
 const siteConfig = {
   title: "DataPoint", // Title for your website.
@@ -39,14 +29,11 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: "introduction/introduction", label: "Docs" },
-    { doc: "api/data-point", label: "API" },
-    { page: "help", label: "Help" },
-    { blog: true, label: "Blog" }
+    { doc: "getting-started", label: "Docs" },
+    { href: "/data-point/jsdoc", label: "API" },
+    { href: "https://github.com/ViacomInc/data-point", label: "GitHub", target: "_blank" }
   ],
-
-  // If you have users set above, you add it here:
-  users,
+  separateCss: ['static/jsdoc/styles'],
 
   /* path to images for header/footer */
   headerIcon: "img/favicon.ico",
