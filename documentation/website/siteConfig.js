1920f1699d67e9eb85a408bc4acb9be0e49520e6
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2019-09-03 23:30:48 +0200

    Restored organization name

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -22,7 +22,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: "data-point",
-  organizationName: "csarnataro",
+  organizationName: "ViacomInc",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
