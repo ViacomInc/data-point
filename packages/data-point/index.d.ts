e50612fc50177b94bf0a7c5828d6d244b94e5bd7
Tomasz fertacz <tomasz.fertacz@vimn.com>
2019-10-12 18:50:35 +0200

    feat: remove semicolons

--- packages/data-point/index.d.ts
+++ packages/data-point/index.d.ts
@@ -1,12 +1,12 @@
 export declare namespace DataPoint {
   interface Accumulator {
-    value: any;
-    locals: object;
-    values: any;
+    value: any
+    locals: object
+    values: any
     reducer: object
-    trace: boolean;
-    context: object;
-    traceGraph: [];
+    trace: boolean
+    context: object
+    traceGraph: []
   }
 
   interface Reducer {}
